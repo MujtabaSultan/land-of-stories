99a885c420cd96f8f4560bcd291593716d989602
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2026-04-13 18:46:02 +0300

    added the di

--- src/main/java/com/stories/stories/controllers/StoryController.java
+++ src/main/java/com/stories/stories/controllers/StoryController.java
@@ -1,10 +1,19 @@
 package com.stories.stories.controllers;
 
 
+import com.stories.stories.services.StoryService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/stories")
 public class StoryController {
+    private final StoryService storyService;
+
+    public StoryController(StoryService storyService) {
+
+        this.storyService = storyService;
+    }
+
+
 }

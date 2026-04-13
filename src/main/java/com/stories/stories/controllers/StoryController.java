c91e92f87c1a247175e23c2f9f3c002571d77881
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2026-04-13 18:42:30 +0300

    the story controller

---
+++ src/main/java/com/stories/stories/controllers/StoryController.java
@@ -0,0 +1,10 @@
+package com.stories.stories.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stories")
+public class StoryController {
+}
